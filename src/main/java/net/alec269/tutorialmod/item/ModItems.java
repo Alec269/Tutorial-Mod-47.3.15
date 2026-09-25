30241ef9ad68dd63ace016872b90fe7ed642c1ca
Alec269 <alac269b@gmail.com>
2026-09-25 20:29:34 +0600

    Custom fuel item

--- src/main/java/net/alec269/tutorialmod/item/ModItems.java
+++ src/main/java/net/alec269/tutorialmod/item/ModItems.java
@@ -1,7 +1,9 @@
 package net.alec269.tutorialmod.item;
 
 import net.alec269.tutorialmod.TutorialMod;
+import net.alec269.tutorialmod.item.custom.FuelItem;
 import net.alec269.tutorialmod.item.custom.MetalDetectorItem;
+
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -16,21 +18,37 @@ public class ModItems {
    // a DeferredRegister is like a list
    
    //# ADD Item
-   public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
-      () -> new Item(new Item.Properties()));
+   public static final RegistryObject<Item> SAPPHIRE = ITEMS.register(
+      "sapphire", () -> new Item(
+         new Item.Properties()
+      )
+   );
    
-   public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
-      () -> new Item(new Item.Properties()));
+   public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register(
+      "raw_sapphire", () -> new Item(
+         new Item.Properties()
+      )
+   );
    
+   // Custom Item ------------------------------------------
    public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register(
       "metal_detector", () -> new MetalDetectorItem(
          new Item.Properties().durability(500)
       )
    );
    
+   // Food ------------------------------------------
    public static final RegistryObject<Item> STRAWBERRY = ITEMS.register(
       "strawberry", () -> new Item(
          new Item.Properties().food(ModFoods.STRAWBERRY)
+      )
+   );
+   
+   // Fuel ------------------------------------------
+   public static final RegistryObject<Item> PINE_CONE = ITEMS.register(
+      "pine_cone", () -> new FuelItem(
+         new Item.Properties(),
+         400
       )
    );
    
